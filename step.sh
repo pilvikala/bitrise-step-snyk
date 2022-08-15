9aa0853c295b000240e9789122b55ced593b2979
pilvikala <michal.brutvan@gmail.com>
2022-08-15 17:58:24 +0300

    additional commandline options

--- step.sh
+++ step.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -ex
 
-npm install -g snyk
+run_test()
+{
+    snyk $1 --severity-threshold=$2 $3 $4
+}
+
+npm install --location=global snyk
 
 snyk auth ${auth_token}
 
+target_file_arg=""
+org_arg=""
 
 
-snyk ${command} --severity-threshold=${severity_threshold} --fail-on-issues=${fail_on_issues}
+[[ ! -z "$target_file" ]] && target_file_arg="--file=${target_file}"
+[[ ! -z "$organization" ]] && org_arg="--org=${organization}"
+
+if [[ "${fail_on_issues}" == "true" ]]; then
+    run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments}
+else
+    run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments} || true
+fi
 
 if [[ "${monitor}" == "true" ]]; then
-    snyk monitor
+    snyk monitor ${target_file_arg} ${org_arg} ${additional_arguments}
 fi
