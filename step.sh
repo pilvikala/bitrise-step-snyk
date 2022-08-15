1ee643243607a48d3719a125c0219afc20f3c1ff
pilvikala <michal.brutvan@gmail.com>
2022-08-15 10:00:33 +0300

    fix if condition to run monitor command

--- step.sh
+++ step.sh
@@ -9,6 +9,6 @@ snyk auth ${auth_token}
 
 snyk ${command} --severity-threshold=${severity_threshold} --fail-on-issues=${fail_on_issues}
 
-if [["${monitor}" == "true"]]; then
+if [[ "${monitor}" == "true" ]]; then
     snyk monitor
 fi
