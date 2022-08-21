16e3454b6806beedcbf7560cebc451390ea1698e
pilvikala <michal.brutvan@gmail.com>
2022-08-21 10:14:01 +0300

    PR-suggested improvements

--- step.sh
+++ step.sh
@@ -23,12 +23,12 @@ org_arg=""
 [[ ! -z "$target_file" ]] && target_file_arg="--file=${target_file}"
 [[ ! -z "$organization" ]] && org_arg="--org=${organization}"
 
-if [[ "${fail_on_issues}" == "true" ]]; then
+if [[ "${fail_on_issues}" == "yes" ]]; then
     run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments}
 else
     run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments} || true
 fi
 
-if [[ "${monitor}" == "true" ]]; then
+if [[ "${monitor}" == "yes" ]]; then
     snyk monitor ${target_file_arg} ${org_arg} ${additional_arguments}
 fi
