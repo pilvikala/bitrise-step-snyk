e6c722eba39e83faddb8817b68b512484e2ec4f8
pilvikala <michal.brutvan@gmail.com>
2022-09-02 18:15:15 +0300

    html report generation

--- step.sh
+++ step.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 set -ex
+set -o pipefail
 
 run_test()
 {
-    snyk $1 --severity-threshold=$2 $3 $4
+  if [[ "$3" == "yes" ]]; then
+    snyk $1 --severity-threshold=$2 $4 $5 --json | snyk-to-html -o snyk_report.html
+  else
+    snyk $1 --severity-threshold=$2 $4 $5
+  fi
 }
 
-npm install --location=global snyk
+packages="snyk"
+
+if [[ "${create_report}" == "yes" ]]; then
+  packages="snyk snyk-to-html"
+fi
+
+npm install --location=global ${packages}
 
 if [[ ${command} != "test_this_step" ]]; then
   snyk auth ${auth_token}
@@ -18,15 +29,15 @@ fi
 
 target_file_arg=""
 org_arg=""
-
+create_report_arg=""
 
 [[ ! -z "$target_file" ]] && target_file_arg="--file=${target_file}"
 [[ ! -z "$organization" ]] && org_arg="--org=${organization}"
 
 if [[ "${fail_on_issues}" == "yes" ]]; then
-    run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments}
+    run_test ${command} ${severity_threshold} ${create_report} ${target_file_arg} ${additional_arguments}
 else
-    run_test ${command} ${severity_threshold} ${target_file_arg} ${additional_arguments} || true
+    run_test ${command} ${severity_threshold} ${create_report} ${target_file_arg} ${additional_arguments} || true
 fi
 
 if [[ "${monitor}" == "yes" ]]; then
