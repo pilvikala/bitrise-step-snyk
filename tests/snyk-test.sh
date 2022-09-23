d828c7736bc928613d4823a59e9e45f6c0d7100c
pilvikala <michal.brutvan@gmail.com>
2022-09-23 15:31:12 +0300

    add tests

--- tests/snyk-test.sh
+++ tests/snyk-test.sh
@@ -6,10 +6,18 @@ fi
 
 command=test
 severity_threshold=low
-fail_on_issues=true
+fail_on_issues=yes
 create_report=no
 monitor=no
-target_file=
+target_file=tests/test-data/package-lock.json
 organization=
 
 auth_token=$SNYK_AUTH_TOKEN command=$command severity_threshold=$severity_threshold fail_on_issues=$fail_on_issues create_report=$create_report monitor=$monitor target_file=$target_file organization=$organization ./step.sh
+exit_code=$?
+if [[ $exit_code -ne 1 ]]; then
+    echo "Failed Snyk Test test. Returned code: $exit_code"
+    exit 1
+else
+    echo "Test passed"
+    exit 0
+fi
